d58662ff8790d70cb27f3db2c574465f8d0fb242
DK Design <dkdesign254@gmail.com>
2026-04-18 17:10:37 +0300

    Enforce authenticated access for logs and reports

--- server/routers/logs.ts
+++ server/routers/logs.ts
@@ -5,26 +5,26 @@ import {
   listAllReports,
   getDashboardStats,
 } from "../db";
-import { publicProcedure, router } from "../_core/trpc";
+import { protectedProcedure, router } from "../_core/trpc";
 
 export const logsRouter = router({
-  executionLogs: publicProcedure
+  executionLogs: protectedProcedure
     .input(z.object({ workflowId: z.string() }))
     .query(async ({ input }) => {
       return listExecutionLogsByWorkflow(input.workflowId);
     }),
 
-  aiLogs: publicProcedure
+  aiLogs: protectedProcedure
     .input(z.object({ workflowId: z.string() }))
     .query(async ({ input }) => {
       return listAILogsByWorkflow(input.workflowId);
     }),
 
-  allReports: publicProcedure.query(async () => {
+  allReports: protectedProcedure.query(async () => {
     return listAllReports();
   }),
 
-  dashboardStats: publicProcedure.query(async () => {
+  dashboardStats: protectedProcedure.query(async () => {
     return getDashboardStats();
   }),
 });
